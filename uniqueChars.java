41133e79c9121250eec08e77d3a3bc1f875fb3c0
Marcos Valle <marcos.valle01@gmail.com>
2016-10-26 07:43:56 -0200

    Update uniqueChars.java

--- uniqueChars.java
+++ uniqueChars.java
@@ -7,7 +7,7 @@ public class uniqueChars{
 	public static void main(String[] args){
 		String str = "abcdef";
 		int i;
-		boolean[] alphaBit = new boolean[256];
+		boolean[] alphaBit = new boolean[256]; //Considering ASCII chars.
 		boolean res=true;
 
 		for(i=0 ; i<str.length() ; i++){
@@ -28,6 +28,7 @@ complexity:
 	space: O(n)
 
 #### OTHER SOLUTIONS ####
+	Do the same stuff but WITHOUT ALLOCATING THE BOOLEAN ARRAY.
 	Compare each char with all the others (O(n²)) time and no space.
 	Sort the string O(log n) and then check the string for each neighboring char O(n) -> O(n log n)
 	(???) Sort the string and do a binary search O(log n) for each char O(n) -> O(n log n)
@@ -37,4 +38,8 @@ Does java does automatic char->int casting?
 	Yes.
 Does java have a bitArray-like data structure?
 	Yes. Check BitSet.
+How would it change for Unicode chars?
+
+#### TODO ####
+Create unitTests
 */
